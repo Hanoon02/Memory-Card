a481bf1a0a659746664593751db499cae0d0569c
Hanoon02 <ahmedhanoon02@gmail.com>
2022-09-21 21:15:19 +0530

    tailwind add

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <>
+            <div className="text-center text-4xl pt-5">
+                <h2>Fruit Memory</h2>
+            </div>
+        </>
+    )
+}
+
+export default Header;
